fdbaf7c2735065bf6ef10e787341e937167af410
Josh Wang <Joshw9012@gmail.com>
2023-11-02 18:11:15 +1100

    fixed the text line msg

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -11,6 +11,7 @@ class Header extends Component {
     const name = this.props.data.name;
     const description = this.props.data.description;
     const description1 = this.props.data.description1;
+    const description2 = this.props.data.description2;
 
     return (
       <header id="home">
